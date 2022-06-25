7227afea9f23ffb3b6d1508198b1ba4501fc365d
den_hope <den_hope@mail.ru>
2022-06-26 00:34:30 +0300

    feat: add input validate

---
+++ src/hooks/useInput.ts
@@ -0,0 +1,16 @@
+import { ChangeEvent, useState } from 'react';
+import { useValidation } from './useValidation';
+
+export const useInput = (initialValue: string, validations: {}) => {
+  const [value, setValue] = useState<string>(initialValue);
+  const [isDirty, setDirty] = useState<boolean>(false);
+  const valid = useValidation(value, validations);
+
+  const onChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>) => {
+    setValue(e.currentTarget.value);
+  };
+  const onBlur = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>) => {
+    setDirty(true);
+  };
+  return { value, onChange, onBlur, isDirty, ...valid };
+};
---
+++ src/hooks/useValidation.ts
@@ -0,0 +1,75 @@
+import { useEffect, useState } from 'react';
+
+export const useValidation = (value: string, validations: any) => {
+  const [isEmpty, setEmpty] = useState<boolean>(true);
+  const [minLengthError, setMinLengthError] = useState<boolean>(false);
+  const [maxLengthError, setMaxLengthError] = useState<boolean>(false);
+  const [valueTypeError, setValueTypeError] = useState<boolean>(false);
+  const [isEmailTypeError, setEmailTypeError] = useState<boolean>(false);
+  const [isTelTypeError, setTelTypeError] = useState<boolean>(false);
+  const [inputValid, setInputValidError] = useState<boolean>(false);
+
+  useEffect(() => {
+    for (const validation in validations) {
+      switch (validation) {
+        case 'isEmpty':
+          value ? setEmpty(false) : setEmpty(true);
+          break;
+
+        case 'minLength':
+          value.length < validations[validation]
+            ? setMinLengthError(true)
+            : setMinLengthError(false);
+          break;
+        case 'maxLength':
+          value.length > validations[validation]
+            ? setMaxLengthError(true)
+            : setMaxLengthError(false);
+          break;
+        case 'isName':
+          const resName = /^([a-zA-Z]{3,})+\s{1}([a-zA-Z]{3,})+$/i;
+          resName.test(String(value).toLocaleLowerCase())
+            ? setValueTypeError(false)
+            : setValueTypeError(true);
+          break;
+        case 'isEmailType':
+          const resEmail = /^([a-z0-9_\.-]+)@([a-z0-9_\.-]+)\.([a-z\.]{2,6})$/i;
+          resEmail.test(String(value).toLocaleLowerCase())
+            ? setEmailTypeError(false)
+            : setEmailTypeError(true);
+          break;
+        case 'isTelType':
+          const resTel = /^\d{3}\d{3}\d{2}\d{2}$/i;
+          resTel.test(String(value).toLocaleLowerCase())
+            ? setTelTypeError(false)
+            : setTelTypeError(true);
+          break;
+      }
+    }
+  }, [value]);
+
+  useEffect(() => {
+    if (
+      isEmpty ||
+      maxLengthError ||
+      minLengthError ||
+      valueTypeError ||
+      isEmailTypeError ||
+      isTelTypeError
+    ) {
+      setInputValidError(false);
+    } else {
+      setInputValidError(true);
+    }
+  }, [isEmpty, minLengthError, maxLengthError, valueTypeError, isEmailTypeError, isTelTypeError]);
+
+  return {
+    isEmpty,
+    minLengthError,
+    maxLengthError,
+    valueTypeError,
+    isEmailTypeError,
+    isTelTypeError,
+    inputValid,
+  };
+};
